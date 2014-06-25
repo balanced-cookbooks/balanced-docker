2ce4356d77bf878679310cb765c99a8d83b641d2
Victor Lin <bornstub@gmail.com>
2014-06-24 19:13:21 -0700

    Add docker repo configuration

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['balanced-docker']['repo_url'] = 'https://index.docker.io/v1/'
+# FIXME: just for temporary development only, we may should register a proper
+# account later
+default['balanced-docker']['email'] = 'victorlin+docker.balanceddeploy@balancedpayments.com'
+default['balanced-docker']['username'] = 'balanceddeploy'
+default['balanced-docker']['password_file'] = 'docker/balanceddeploy_password'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,9 @@
 include_recipe "balanced-apt"
 include_recipe "docker"
+
+
+docker_registry node['balanced-docker']['repo_url'] do
+  email node['balanced-docker']['email']
+  username node['balanced-docker']['username']
+  password citadel[node['balanced-docker']['password_file']]
+end
