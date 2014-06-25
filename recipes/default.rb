eaee17ff808df96466f781927d07fa3acd2f22d9
Victor Lin <bornstub@gmail.com>
2014-06-25 13:33:05 -0700

    Bump version for release

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,5 @@ include_recipe "docker"
 docker_registry node['balanced-docker']['repo_url'] do
   email node['balanced-docker']['email']
   username node['balanced-docker']['username']
-  password citadel[node['balanced-docker']['password_file']]
+  password citadel[node['balanced-docker']['password_file']].chomp
 end
